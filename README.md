ad9ae1f4a4cf83b71ff2c25894fbf2ae6cdb3f4a
rogeriocassares <roger10cassares@gmail.com>
2023-06-17 23:07:06 -0300

    stage 5 web

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # rocketseat-nlw-12-spacetime-web
 
-Stage 4 END
+Stage 5 00:22
---
+++ src/components/MediaPicker.tsx
@@ -0,0 +1,49 @@
+'use client'
+
+import { ChangeEvent, useState } from 'react'
+
+export function MediaPicker() {
+  const [preview, setPreview] = useState<string | null>(null)
+
+  function onFileSelected(event: ChangeEvent<HTMLInputElement>) {
+    console.log(event.target.files)
+    const { files } = event.target
+
+    if (!files) {
+      return
+    }
+
+    const previewUrl = URL.createObjectURL(files[0])
+
+    setPreview(previewUrl)
+  }
+
+  return (
+    <>
+      <input
+        onChange={onFileSelected}
+        type="file"
+        id="media"
+        accept="image/*"
+        className="invisible h-0 w-0"
+      />
+
+      {preview && (
+        // The image is not on server. It does not make sense to optimize. So disable the ESLint
+        // eslint-disable-next-line
+        <img
+          src={preview}
+          alt=""
+          className="aspect-video w-full rounded-lg object-cover"
+        />
+
+        // <video
+        //   src={preview}
+        //   alt=""
+        //   className="aspect-video w-full rounded-lg object-cover"
+        //   controls={false}
+        // />
+      )}
+    </>
+  )
+}
